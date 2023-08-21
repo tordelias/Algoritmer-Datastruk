dc64f33471e45eb2af9b8daf6cbb6b26f8b2af50
tordelias <tordeliashoumb46@gmail.com>
2023-08-22 01:11:18 +0200

    Finished task1 & 2 compulsory 1
    finished it i belive

--- Compulsory 1/Compulsory 1/Compulsory 1/ADS.cpp
+++ Compulsory 1/Compulsory 1/Compulsory 1/ADS.cpp
@@ -9,7 +9,10 @@ int main()
 
 	long double n;
 	std::cin >> n;
+
 	recursive.Result = 1;
+	recursive.Result1 = 1;
+	recursive.Result2= 1;
 	long double r = recursive.Recursive(n);
 	std::cout << r << std::endl;
 	return 0;
@@ -22,10 +25,20 @@ long double ADS::Recursive(long double X)
 	{
 		return Result;
 	}
+	//factorial//
+	//else
+	//{
+	//	Result *= X;
+	//return	Recursive(--X);
+	//}
+	
+	//fibonacci//
 	else
 	{
-		Result *= X;
-	return	Recursive(--X);
+		Result1 = Result;
+		Result = Result1 + Result2;
+		Result2 = Result1;
+		return	Recursive(--X);
 	}
 
 }
--- Compulsory 1/Compulsory 1/Compulsory 1/ADS.h
+++ Compulsory 1/Compulsory 1/Compulsory 1/ADS.h
@@ -4,5 +4,7 @@ class ADS
 public:
 	long double Recursive( long double X );
 	long double Result;
+	long double Result1;
+	long double Result2;
 };
 
